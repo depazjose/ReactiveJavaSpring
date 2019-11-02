205b9d51d0393be3b4f09e281ea1c80b3c69448d
depazjose <jose.depaz@gmail.com>
2019-11-01 21:59:01 -0500

    added load file from resources

--- infrastructure/src/main/java/mdt/desa/web/task/EmployeeService.java
+++ infrastructure/src/main/java/mdt/desa/web/task/EmployeeService.java
@@ -3,9 +3,17 @@ package mdt.desa.web.task;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import mdt.desa.usecase.SampleUseCase;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+import reactor.util.Logger;
+import reactor.util.Loggers;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping(value = "/employee", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -13,10 +21,12 @@ import reactor.core.publisher.Mono;
 public class EmployeeService {
 
     private final SampleUseCase useCase;
+    Logger logger = Loggers.getLogger(EmployeeService.class);
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, path = "/hello/msn")
     public Mono<Void> saveMsn(@RequestBody MessageData data) {
-        System.out.println("data: " + data.taskId + ", " + data.userId);
+        loadFile();
+        logger.info("data: " + data.taskId + ", " + data.userId);
         return useCase.assignTask(data.taskId, data.userId);
     }
 
@@ -26,4 +36,24 @@ public class EmployeeService {
         private String userId;
     }
 
+    private void loadFile()
+    {
+        Resource resource = new ClassPathResource("Estandar.xml");
+
+        try {
+            InputStream input = resource.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            File file = resource.getFile();
+            logger.info(file.getName());
+            logger.info(file.toString());
+
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+            //e.printStackTrace();
+        }
+    }
 }
